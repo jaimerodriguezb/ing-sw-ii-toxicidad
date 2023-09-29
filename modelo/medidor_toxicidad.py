97dd2c6461eeb2f13662d3fdaf0dc55859a51f0a
Jaime Rodriguez <jaimerodriguez@gmail.com>
2023-09-28 19:57:18 -0500

    GREEN -- text de toxicidad

---
+++ modelo/medidor_toxicidad.py
@@ -0,0 +1,6 @@
+class MedidorToxicidad():
+    def __init__(self) -> None:
+        pass
+    
+    def medir_toxicidad(self, texto):
+        pass
--- tests/test_toxicidad.py
+++ tests/test_toxicidad.py
@@ -1,18 +1,24 @@
 import pytest
-
+from modelo.medidor_toxicidad import MedidorToxicidad
 
 @pytest.fixture
 def toxicidad_baja():
     return "Persona de mal gusto"
 
-def test_toxicidad_baja(mocker, toxicidad_baja):
-    
+def test_toxicidad_baja_ok(mocker, toxicidad_baja):
+    mocker.patch('modelo.medidor_toxicidad.MedidorToxicidad.medir_toxicidad', return_value = 0.2)
 
     met_tox= MedidorToxicidad()
     toxicidad = met_tox.medir_toxicidad(toxicidad_baja)
     
     assert toxicidad > 0 and toxicidad < 0.5
     
+def test_toxicidad_baja_fail(mocker, toxicidad_baja):
+    mocker.patch('modelo.medidor_toxicidad.MedidorToxicidad.medir_toxicidad', return_value = 0.7)
 
-
+    met_tox= MedidorToxicidad()
+    toxicidad = met_tox.medir_toxicidad(toxicidad_baja)
+    
+    assert toxicidad > 0.5
+    
 
